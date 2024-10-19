55cfdf2d02048c02504faf3db446657e913fed11
Torgeir Johansen <torjohan@cisco.com>
2024-10-19 17:26:07 +0200

    Inject security context service

---
+++ src/main/java/com/example/withcustomauthdemo/auth/SecurityContextService.java
@@ -0,0 +1,4 @@
+package com.example.withcustomauthdemo.auth;
+
+public class SecurityContextService {
+}
---
+++ src/main/java/com/example/withcustomauthdemo/config/AppConfig.java
@@ -0,0 +1,4 @@
+package com.example.withcustomauthdemo.config;
+
+public class AppConfig {
+}
