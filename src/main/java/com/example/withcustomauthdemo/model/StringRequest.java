994b5d0d9b57c41751c0a332c859c22dfa1a416c
Torgeir Johansen <torjohan@cisco.com>
2024-10-30 11:59:39 +0100

    More input validation tests

--- src/main/java/com/example/withcustomauthdemo/model/StringRequest.java
+++ src/main/java/com/example/withcustomauthdemo/model/StringRequest.java
@@ -5,5 +5,5 @@ import jakarta.validation.constraints.Size;
 
 public record StringRequest(
     @NotBlank(message = "Content must not be blank")
-    @Size(max = 255, message = "Content must not exceed 255 characters")
+    @Size(max = 255, min = 1, message = "Content length must be between 1-255 characters")
     String content) {}
--- src/test/java/com/example/withcustomauthdemo/model/StringRequestValidationTest.java
+++ src/test/java/com/example/withcustomauthdemo/model/StringRequestValidationTest.java
@@ -23,6 +23,19 @@ public class StringRequestValidationTest {
 
         assertThat(violations).hasSize(1);
         assertThat(violations.toArray()[0]).toString()
-                                           .contains("Content must not exceed 255 characters");
+                                           .contains("Content length must be between 1-255 characters");
+    }
+
+    @Test
+    public void testInputValidation_minLength() {
+        StringRequest stringRequest = new StringRequest("");
+
+        var violations = validator.validate(stringRequest);
+
+        assertThat(violations).hasSize(2);
+        assertThat(violations.toArray()[0]).toString()
+                                           .contains("Content length must be between 1-255 characters");
+        assertThat(violations.toArray()[1]).toString()
+                                           .contains("Content must not be blank");
     }
 }
